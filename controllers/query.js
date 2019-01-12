25e549ba4c6b17f9131493cbf7a80cdce98b63de
dilrong <wet932@naver.com>
2019-01-12 23:24:24 +0900

    query null process

--- controllers/query.js
+++ controllers/query.js
@@ -10,39 +10,28 @@ router.get('/query', async(req, res) => {
 });
 
 router.get('/query/:address', async(req, res) => {
-    const query = await db.query.findOne({
-        where: { queryAddress: req.params.address }
-    });
+    try{
+        const query = await db.query.findOne({
+            where: { queryAddress: req.params.address }
+        });
 
-    return res.json(query);
+        if(query == null){
+            return res.json(false);
+        }else{
+            console.log(query)
+            return res.json(query);
+        }
+    }catch(err){
+        console.log(err);
+    }
 });
 
 router.post('/query', async(req, res) => {
     const query = await db.query.create({
         queryAddress: req.body.queryAddress,
         queryComment: req.body.queryComment,
-        queryContributor: req.body.queryContributor
+        queryContributor: req.body.queryContributor,
     });
-
-    const queryReported = await db.query.findOne({
-        where: { queryAddress: query.queryAddress }
-    });
-    let Point;
-
-    switch(query.queryComment){
-        case 1:
-            Point = 5*queryReported;
-        break;
-
-        case 2:
-            Point = 10*queryReported;
-        break;
-
-        default:
-            res.json('error');
-        break;
-    }
-
     return res.json(true);
 });
 
