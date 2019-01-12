23b55f6e83ac9363dadf9937136c7ebcb1b04725
dilrong <wet932@naver.com>
2019-01-12 20:09:09 +0900

    update query get and post post - point get /query/:address

--- controllers/query.js
+++ controllers/query.js
@@ -9,15 +9,41 @@ router.get('/query', async(req, res) => {
     return res.json(query);
 });
 
+router.get('/query/:address', async(req, res) => {
+    const query = await db.query.findOne({
+        where: { queryAddress: req.params.address }
+    });
+
+    return res.json(query);
+});
+
 router.post('/query', async(req, res) => {
     const query = await db.query.create({
         queryAddress: req.body.queryAddress,
         queryComment: req.body.queryComment,
-        queryContributor: req.body.queryContributor,
-        queryPoint: req.body.queryPoint,
+        queryContributor: req.body.queryContributor
     });
 
-    return res.json(query);
+    const queryReported = await db.query.findOne({
+        where: { queryAddress: query.queryAddress }
+    });
+    let Point;
+
+    switch(query.queryComment){
+        case 1:
+            Point = 5*queryReported;
+        break;
+
+        case 2:
+            Point = 10*queryReported;
+        break;
+
+        default:
+            res.json('error');
+        break;
+    }
+
+    return res.json(true);
 });
 
 export default router;
